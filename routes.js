677e31a6bd66d59947b404f05e3bcfd961e893aa
Gustavo Martins <gustavo_sh8@hotmail.com>
2016-12-06 19:29:52 -0200

    Rota do consultar.html

--- routes.js
+++ routes.js
@@ -7,5 +7,9 @@ module.exports = function(app) {
 
     app.get("/adicionar", function(request, response) {
         response.sendFile(path + "adicionar.html");
+    });
+    
+    app.get("/consultar", function(request, response) {
+        response.sendFile(path + "consultar.html");
     });    
 };
