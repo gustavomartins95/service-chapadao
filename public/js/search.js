e20c5ef1cc9ec2503ad54005037f84dd02564be5
Gustavo Martins <gustavo_sh8@hotmail.com>
2016-12-06 20:48:37 -0200

    Validando dados não encontrados

--- public/js/search.js
+++ public/js/search.js
@@ -9,38 +9,47 @@
             });
     };
 
-    function addTableRow(services) {
+function addTableRow(services) {
         var newRow, columns, total;
 
-        for (var i in services) {
-            total = parseFloat(services[i].maoDeObra) + parseFloat(services[i].preco);
-
+        if (services.length === 0) {
             newRow = $("<tr>");
             columns = "";
-
-            columns += "<td>" + services[i].id + "</td>";
-            columns += "<td>" + services[i].nome + "</td>";
-            columns += "<td>" + services[i].placa + "</td>";
-            columns += "<td>" + services[i].modelo + "</td>";
-            columns += "<td>" + services[i].formasDePagamento + "</td>";
-            columns += "<td>" + services[i].preco.toFixed(2) + "</td>";
-            columns += "<td>" + services[i].maoDeObra.toFixed(2) + "</td>";
-            columns += "<td>" + total.toFixed(2) + "</td>";
-            columns += "<td>" + services[i].dataAtual + "</td>";
-            columns += "<td>" + services[i].dataDeVencimento + "</td>";
-
-            columns += '<td class="actions">';
-            columns += '<a class="btn btn-success btn-xs" href="#">Receber</a> <a class="btn btn-primary btn-xs" href="#">Visualizar</a> <a class="btn btn-warning btn-xs" href="#">Editar</a> <a class="btn btn-danger btn-xs" href="#">Excluir</a>';
-            columns += '</td>';
-
+            columns += '<td colspan="12" class="text-center danger">Nenhum dado encontrado.</td>';
             newRow.append(columns);
             $("#tableServices").append(newRow);
+        } else {
+            for (var i in services) {
+                total = parseFloat(services[i].maoDeObra) + parseFloat(services[i].preco);
+
+                newRow = $("<tr>");
+                columns = "";
+
+                columns += "<td>" + services[i].id + "</td>";
+                columns += "<td>" + services[i].nome + "</td>";
+                columns += "<td>" + services[i].placa + "</td>";
+                columns += "<td>" + services[i].modelo + "</td>";
+                columns += "<td>" + services[i].formasDePagamento + "</td>";
+                columns += "<td>" + services[i].preco.toFixed(2) + "</td>";
+                columns += "<td>" + services[i].maoDeObra.toFixed(2) + "</td>";
+                columns += "<td>" + total.toFixed(2) + "</td>";
+                columns += "<td>" + services[i].dataAtual + "</td>";
+                columns += "<td>" + services[i].dataDeVencimento + "</td>";
+
+                columns += '<td class="actions">';
+                columns += '<a class="btn btn-success btn-xs" href="#">Receber</a> <a class="btn btn-primary btn-xs" href="#">Visualizar</a> <a class="btn btn-warning btn-xs" href="#">Editar</a> <a class="btn btn-danger btn-xs" href="#">Excluir</a>';
+                columns += '</td>';
+
+                newRow.append(columns);
+                $("#tableServices").append(newRow);
+            }
         }
         return false;
-    };
+    };    
 
     $("form").on("submit", event => {
         let search = $("#search").val();
+        $("#tableServices tr").remove();
         getService(search);
         return false;
     });
