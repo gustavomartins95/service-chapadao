749ea1a5893bc19cc4a7816eeb04ec69f7198d4a
Douglas Oliveira <douglinhas_uchiha@hotmail.com>
2016-12-06 19:38:10 -0200

    Rota do cadastrar

--- routes.js
+++ routes.js
@@ -8,8 +8,12 @@ module.exports = function(app) {
     app.get("/adicionar", function(request, response) {
         response.sendFile(path + "adicionar.html");
     });
+    app.post("/cadastrar", function (request, response) {
+        console.log(request.body);
+        response.send("Cadastrado.");
+    });
     
     app.get("/consultar", function(request, response) {
         response.sendFile(path + "consultar.html");
-    });    
+    });
 };
